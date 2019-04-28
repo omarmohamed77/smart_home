f7a08a74ac64c4aa6e5d8f898cdf222bc291732d
AmrHosam <amrhossam97@gmail.com>
2019-04-28 14:39:52 +0200

    one step function

--- drivers/stepper_motor/stepper.c
+++ drivers/stepper_motor/stepper.c
@@ -5,3 +5,37 @@ void stepper_init()
     Port_Init(3);   //initializing port D
     Port_SetPinDirection(3, 0x0F,PORT_PIN_OUT);
 }
+void OneStep(enum direction dir)
+{
+	switch(step_no)
+	{
+		case 0:		GPIO_PORTD_DATA_R = 0x0E;
+							break;
+		case 1:		GPIO_PORTD_DATA_R = 0x0C;
+							break;
+		case 2:		GPIO_PORTD_DATA_R = 0x0D;
+							break;
+		case 3:		GPIO_PORTD_DATA_R = 0x09;
+							break;
+		case 4:		GPIO_PORTD_DATA_R = 0x0B;
+							break;
+		case 5:		GPIO_PORTD_DATA_R = 0x03;
+							break;
+		case 6:		GPIO_PORTD_DATA_R = 0x07;
+							break;
+		case 7:		GPIO_PORTD_DATA_R = 0x06;
+							break;
+		default:	GPIO_PORTD_DATA_R = 0x0F;
+							break;
+	}
+	if(dir)
+	{
+		step_no = (step_no + 1)%8; 
+	}
+	else
+	{
+		step_no = step_no - 1;
+		if(step_no < 0)
+			step_no = 7;
+	}
+}
--- drivers/stepper_motor/stepper.h
+++ drivers/stepper_motor/stepper.h
@@ -1,5 +1,7 @@
 #ifndef STEPPER_H_
 #define STEPPER_H_
 #include "port.h"
+enum direction {CCW,CW};
 void stepper_init();
+void OneStep(enum direction dir);
 #endif /* STEPPER_H_ */
