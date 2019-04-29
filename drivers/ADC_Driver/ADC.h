d833ed1f73a0ced03f58533a5d7c2e074dd57a0f
Omar Shafik <o.shafik13@gmail.com>
2019-04-29 11:54:37 +0200

    syntax fixes

--- drivers/ADC_Driver/ADC.h
+++ drivers/ADC_Driver/ADC.h
@@ -3,14 +3,12 @@
 #ifndef ADC_H_
 #define ADC_H_
 
+#include "Common_Macros.h"
 #include "std_types.h"
 #include "tm4c123gh6pm.h"
-#include "Common_Macros.h"
-
 
 void ADC0_Init();
 
-uint16 ADC0_Reading(uint16 data);
-
+uint16 ADC0_Reading();
 
 #endif /* ADC_H_ */
