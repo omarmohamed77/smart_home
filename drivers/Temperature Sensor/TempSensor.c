4cdd5682841217fa15d3f00373ee87e59bbcf062
Omar Shafik <o.shafik13@gmail.com>
2019-05-04 13:19:51 +0200

    temperature sensor fix

--- drivers/Temperature Sensor/TempSensor.c
+++ drivers/Temperature Sensor/TempSensor.c
@@ -1,7 +1,8 @@
 #include "TempSensor.h"
 
 uint16 get_temperature() {
-    Set_Bit(ADC0_SSCTL0_R, ADC_SSCTL0_TS0);
+    Set_Bit(ADC0_SSCTL3_R, ADC_SSCTL3_TS0);
     uint16 adc_reading = ADC0_Reading();
     uint16 temp = 147.5 - ((75 * (VREFP - VREFN) * adc_reading) / 4096);
+    Clear_Bit(ADC0_SSCTL0_R, ADC_SSCTL0_TS0);
 }
