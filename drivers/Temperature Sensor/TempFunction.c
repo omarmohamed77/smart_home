f8f98612628b8fff6086c91ba85b56bd7412d676
Omar Shafik <o.shafik13@gmail.com>
2019-05-07 22:50:23 +0200

    mod and fixes to ADC and Temp Sensor

--- drivers/Temperature Sensor/TempFunction.c
+++ drivers/Temperature Sensor/TempFunction.c
@@ -1,38 +1,36 @@
 #include "tm4c123gh6pm.h"
-void SystemInit(void){
-}
-volatile int temperature,delay;
+void SystemInit(void) {}
+volatile int temperature, delay;
 float Vresult;
-void ADC_init(void)
-{
-	SYSCTL_RCGCGPIO_R |= 0x10;
-	delay = 0;
-	SYSCTL_RCGCADC_R |= 0x1;
-	delay = 0;
-	delay = 0;
-	delay = 0;
-	//GPIO config.
-	//GPIO_PORTE_AFSEL_R |= 0x8;
-	//GPIO_PORTE_DEN_R &= ~0x8;
-	//GPIO_PORTE_AMSEL_R |= 0x8;
-	//ADC config.
-	ADC0_ACTSS_R &= ~0x8; //disable ss3
-	ADC0_EMUX_R &= ~0xF000; //software trigger
-	//ADC0_SSMUX3_R = 0x0; //read from channel 0 (PE3)
-	ADC0_SSCTL3_R |= 0x6; //single ended, one sample at a time, interrupt enable
-	ADC0_SSCTL3_R |= (1<<3); //read from temp. sensor
-	ADC0_ACTSS_R |= 0x8; //enable ss3
+void ADC_init(void) {
+    SYSCTL_RCGCGPIO_R |= 0x10;
+    delay = 0;
+    SYSCTL_RCGCADC_R |= 0x1;
+    delay = 0;
+    delay = 0;
+    delay = 0;
+    // GPIO config.
+    // GPIO_PORTE_AFSEL_R |= 0x8;
+    // GPIO_PORTE_DEN_R &= ~0x8;
+    // GPIO_PORTE_AMSEL_R |= 0x8;
+    // ADC config.
+    ADC0_ACTSS_R &= ~0x8;   // disable ss3
+    ADC0_EMUX_R &= ~0xF000; // software trigger
+    // ADC0_SSMUX3_R = 0x0; //read from channel 0 (PE3)
+    ADC0_SSCTL3_R |= 0x6; // single ended, one sample at a time, interrupt
+                          // enable
+    ADC0_SSCTL3_R |= (1 << 3); // read from temp. sensor
+    ADC0_ACTSS_R |= 0x8;       // enable ss3
 }
-int main(void)
-{
-	ADC_init();
-	//reading
-	while(1)
-	{
-		ADC0_PSSI_R |= 0x8; //take a sample
-		while((ADC0_RIS_R & 0x8) == 0){}
-		Vresult = (ADC0_SSFIFO3_R)*3.3/4095;
-		temperature = ((2.7-Vresult)*75)-55;
-		ADC0_ISC_R = 0x8;
-	}
+int main(void) {
+    ADC_init();
+    // reading
+    while (1) {
+        ADC0_PSSI_R |= 0x8; // take a sample
+        while ((ADC0_RIS_R & 0x8) == 0) {
+        }
+        Vresult = (ADC0_SSFIFO3_R)*3.3 / 4095;
+        temperature = ((2.7 - Vresult) * 75) - 55;
+        ADC0_ISC_R = 0x8;
+    }
 }
--- drivers/Temperature Sensor/TempSensor.c
+++ drivers/Temperature Sensor/TempSensor.c
@@ -1,20 +1,19 @@
 #include "TempSensor.h"
 
-uint16 get_temperature()
-{
-	 uint16 adc_reading;
-	 uint16 temp;
-	 uint16 result;
+uint16 get_temperature() {
+    uint16 adc_reading;
+    uint16 temp;
+    uint16 result;
     ADC0_ACTSS_R &= ~0x8; // disable sequencer 3
     Set_Bit(ADC0_SSCTL3_R, ADC_SSCTL3_TS0);
 
-    ADC0_ACTSS_R |= 0x8; // Enable Sequence 3
-    //ADC0_ACTSS_R &= ~0x8; // disable sequencer 3
+    ADC0_ACTSS_R |= 0x8; // Enable Sequencer 3
     ADC0_Reading(&adc_reading);
-	  result= (adc_reading)*3.3/4095;
-	  temp =((2.7-result)*75)-55;
-   // temp = 147 - ((uint16)(75 * (VREFP - VREFN) * adc_reading) >> 12);
-    Clear_Bit(ADC0_SSCTL0_R, ADC_SSCTL0_TS0);
-    ADC0_ACTSS_R |= 0x8; // Enable Sequence 3
+    result = (adc_reading)*3.3 / 4095;
+    temp = ((2.7 - result) * 75) - 55;
+    // temp = 147 - ((uint16)(75 * (VREFP - VREFN) * adc_reading) >> 12);
+    ADC0_ACTSS_R &= ~0x8; // disable sequencer 3
+    Clear_Bit(ADC0_SSCTL3_R, ADC_SSCTL3_TS0);
+    ADC0_ACTSS_R |= 0x8; // Enable Sequencer 3
     return temp;
 }
