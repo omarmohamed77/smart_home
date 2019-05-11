c62386f6491001cc1d0193623dd80eb7d7038e96
omarmagdy217 <omr.magdy217@gmail.com>
2019-05-11 23:45:35 +0200

    Temperature and Stepper Communication

--- drivers/Tiva_Home_Main/main.c
+++ drivers/Tiva_Home_Main/main.c
@@ -4,32 +4,34 @@
 #define Stepper_Read		0xC0
 
 void SystemInit(void){
+	UART7_init();
 	ADC0_init();
-	UART0_init();
 	Systick_init();
 	stepper_init();
 	//PWM_Init(potentiometer_reading);
 }
 
 int main(void){
-	uint8 Prev_temp=0;
-	uint8 In_Data, Stepper_Data;
+	uint8 Prev_temp=0, avg, preavg=0;
+	uint8 In_Data, Stepper_Data, Out_Data;
 	while(1) {
-		if(get_temperature()!=Prev_temp)
-		{
-			//send
-			if((UART0_FR_R & 0x20)==0)
-			{
-				UART0_DR_R = get_temperature();
-				Prev_temp = get_temperature();
-			}
-		}
+	    Out_Data = get_temperature();
+        SysTick_Wait10ms(10);
+	    avg = (Out_Data+Prev_temp)/2;
+	    if(avg!=preavg)
+	    {
+	        if((UART7_FR_R & 0x20)==0){
+	                UART7_DR_R = Out_Data;
+					Prev_temp = Out_Data;
+	                preavg = avg;
+	        }
+	    }
 		else
 		{
 			//receive
-			if((UART0_FR_R & 0x10)==0)
+			if((UART7_FR_R & 0x10)==0)
 			{
-					In_Data  = (UART0_DR_R & 0xff);
+					In_Data  = (UART7_DR_R & 0xFF);
 					Stepper_Data = In_Data & Stepper_Read;
 					Stepper_Data >>= 6;
 					if(Stepper_Data == 0b01)
