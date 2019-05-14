367380ebbfa9194cdc0c592927d7d2543dc174fa
omar fathey elsisy <31731092+omarElsisy@users.noreply.github.com>
2019-05-14 15:26:20 +0200

    u

--- drivers/PMW width modulation/pwm.c
+++ drivers/PMW width modulation/pwm.c
@@ -6,11 +6,9 @@ void PWM_Init(void)
 
     SYSCTL_RCC_R &= ~(SYSCTL_RCC_USEPWMDIV); //use system clock as PWM clock
 
-    SYSCTL_RCGC0_R |= (1 << 20);     //activate PWM module 0 clock
+    SYSCTL_RCGC0_R |= (1 << 21);     //activate PWM module 0 clock
     SYSCTL_RCGC2_R |= (1 << 4);      //activate port E clock
-    delay = SYSCTL_RCGC2_R;            //allow time for clock to stabalize
-    delay = SYSCTL_RCGC2_R;            //allow time for clock to stabalize
-    delay = SYSCTL_RCGC2_R;            //allow time for clock to stabalize
+  delay = SYSCTL_RCGC2_R;            //allow time for clock to stabalize
     GPIO_PORTE_DR4R_R |= (1 << 4);
     GPIO_PORTE_DEN_R |= (1 << 4);      //enable digital output on PE4
     GPIO_PORTE_AFSEL_R |= (1 << 4);      //activate  Alternate function on PE4
@@ -21,7 +19,7 @@ void PWM_Init(void)
     PWM1_2_LOAD_R = (5000 - 1);       //5000 (16khz pwm)
     PWM1_2_CMPA_R = (2500 - 1); //set couter value at which CmpA triggers (any value now)
     PWM1_2_CTL_R |= (0x01);         //enable pwm block
-    PWM1_ENABLE_R |= (PWM_ENABLE_PWM0EN);         //Enable pwm output to M0PWM0
+    PWM1_ENABLE_R |= (PWM_ENABLE_PWM1EN);         //Enable pwm output to M1PWM2
 }
 void PWM_Modulation(int potiometer_reading)
 {
@@ -32,5 +30,5 @@ void PWM_Modulation(int potiometer_reading)
     PWM1_2_LOAD_R = (5000 - 1);       //5000 (16khz pwm)
     PWM1_2_CMPA_R = (duty_c); //set couter value at which CmpA triggers (any value now)
     PWM1_2_CTL_R |= (0x01);         //enable pwm block
-    PWM1_ENABLE_R |= (PWM_ENABLE_PWM0EN);         //Enable pwm output to M0PWM0
+    PWM1_ENABLE_R |= (PWM_ENABLE_PWM1EN);         //Enable pwm output to M1PWM2
 }
