474b48049f9257c3288901b31a93f20fa9881421
omar fathey elsisy <31731092+omarElsisy@users.noreply.github.com>
2019-05-14 15:08:28 +0200

    s

--- drivers/PMW width modulation/pwm.c
+++ drivers/PMW width modulation/pwm.c
@@ -15,22 +15,22 @@ void PWM_Init(void)
     GPIO_PORTE_DEN_R |= (1 << 4);      //enable digital output on PE4
     GPIO_PORTE_AFSEL_R |= (1 << 4);      //activate  Alternate function on PE4
     GPIO_PORTE_PCTL_R |= (0x04000000);      //set alternate peripheral to PWM
-    PWM0_2_CTL_R &= ~(1 << 0);         //disable pwm block until ready to use
-    PWM0_2_CTL_R &= ~(1 << 1);         //set count-down mode
-    PWM0_2_GENA_R |= (0xC2); //drive pwm low on CmpA and high on zero(low logic)
-    PWM0_2_LOAD_R = (5000 - 1);       //5000 (16khz pwm)
-    PWM0_2_CMPA_R = (2500 - 1); //set couter value at which CmpA triggers (any value now)
-    PWM0_2_CTL_R |= (0x01);         //enable pwm block
-    PWM0_ENABLE_R |= (PWM_ENABLE_PWM0EN);         //Enable pwm output to M0PWM0
+    PWM1_2_CTL_R &= ~(1 << 0);         //disable pwm block until ready to use
+    PWM1_2_CTL_R &= ~(1 << 1);         //set count-down mode
+    PWM1_2_GENA_R |= (0xC2); //drive pwm low on CmpA and high on zero(low logic)
+    PWM1_2_LOAD_R = (5000 - 1);       //5000 (16khz pwm)
+    PWM1_2_CMPA_R = (2500 - 1); //set couter value at which CmpA triggers (any value now)
+    PWM1_2_CTL_R |= (0x01);         //enable pwm block
+    PWM1_ENABLE_R |= (PWM_ENABLE_PWM0EN);         //Enable pwm output to M0PWM0
 }
 void PWM_Modulation(int potiometer_reading)
 {
     int duty_c = ((2500 * potiometer_reading) / 64);
-    PWM0_2_CTL_R &= ~(1 << 0);         //disable pwm block until ready to use
-    PWM0_2_CTL_R &= ~(1 << 1);         //set count-down mode
-    PWM0_2_GENA_R |= (0xC2); //drive pwm low on CmpA and high on zero(low logic)
-    PWM0_2_LOAD_R = (5000 - 1);       //5000 (16khz pwm)
-    PWM0_2_CMPA_R = (duty_c); //set couter value at which CmpA triggers (any value now)
-    PWM0_2_CTL_R |= (0x01);         //enable pwm block
-    PWM0_ENABLE_R |= (PWM_ENABLE_PWM0EN);         //Enable pwm output to M0PWM0
+    PWM1_2_CTL_R &= ~(1 << 0);         //disable pwm block until ready to use
+    PWM1_2_CTL_R &= ~(1 << 1);         //set count-down mode
+    PWM1_2_GENA_R |= (0xC2); //drive pwm low on CmpA and high on zero(low logic)
+    PWM1_2_LOAD_R = (5000 - 1);       //5000 (16khz pwm)
+    PWM1_2_CMPA_R = (duty_c); //set couter value at which CmpA triggers (any value now)
+    PWM1_2_CTL_R |= (0x01);         //enable pwm block
+    PWM1_ENABLE_R |= (PWM_ENABLE_PWM0EN);         //Enable pwm output to M0PWM0
 }
