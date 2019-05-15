f7cd0799d7ff85fa4b9e88f9cace662319837b53
Omar Shafik <o.shafik13@gmail.com>
2019-05-15 05:51:14 +0200

    minor fixes

--- testMain/BTNsSideTest.c
+++ testMain/BTNsSideTest.c
@@ -12,39 +12,10 @@
 #include "timer.h"
 #include "LCD.h"
 
-void wait_n_check()
-{
-    uint8 quit = 0, inD;
-    uint16 pot_reading, prev_pot = 0;
-    while (!quit)
-    {
-        ADC0_Reading(&pot_reading);
-        pot_reading = pot_reading >> 7;
-        if (pot_reading != prev_pot)
-        {
-            UART7_Send(pot_reading);
-            prev_pot = pot_reading;
-        }
-        SysTick_Wait10ms(10);
-        if (Data_Available_To_Be_Received())
-        {
-            inD = (uint8) (UART7_DR_R & Received_Data_Mask);
-            if (inD == 'c')
-            {
-                quit = 1;
-            }
-            else
-            {
-                LCD_print(inD);
-            }
-        }
-    }
-}
-
 int main(void)
 {
     uint8 flag1 = 0, flag2 = 0, led = 0, d1, d2;
-    uint8 r, l, p, inD;
+    uint8 r, l, p, inD, lock = 0;
     uint16 pot_reading, prev_pot = 0;
     ADC0_Init();
     Port_Init(5);
@@ -66,7 +37,14 @@ int main(void)
         if (Data_Available_To_Be_Received())
         {
             inD = (uint8) (UART7_DR_R & Received_Data_Mask);
-            LCD_print(inD);
+            if (inD == 'c')
+            {
+                lock = 0;
+            }
+            else
+            {
+                LCD_print(inD);
+            }
         }
         if (flag1 && DIO_ReadPort(5, 0x01))
         {
@@ -76,50 +54,39 @@ int main(void)
         {
             flag2 = 0;
         }
-        if (!DIO_ReadPort(5, 0x01) && !flag1)
+        if (!DIO_ReadPort(5, 0x01) && !flag1 && !lock)
         {
             SysTick_Wait10ms(3);
             if (!DIO_ReadPort(5, 0x01))
             {
                 led &= ~0x04;
                 led ^= 0x02;
-                r = 1;
+                UART7_Send('r');
                 flag1 = 1;
+                lock = 1;
             }
         }
-        if (!DIO_ReadPort(5, 0x10) && !flag2)
+        if (!DIO_ReadPort(5, 0x10) && !flag2 && !lock)
         {
             SysTick_Wait10ms(3);
             if (!DIO_ReadPort(5, 0x10))
             {
                 led &= ~0x02;
                 led ^= 0x04;
-                l = 1;
+                UART7_Send('l');
                 flag2 = 1;
+                lock = 1;
             }
         }
         if (pot_reading != prev_pot)
         {
             prev_pot = pot_reading;
-            p = 1;
-        }
-        if (r)
-        {
-            r = 0;
-            UART7_Send('r');
-            wait_n_check();
-        }
-        if (l)
-        {
-            l = 0;
-            UART7_Send('l');
-            wait_n_check();
-        }
-        if (p)
-        {
-            p = 0;
             UART7_Send(pot_reading);
         }
-        GPIO_PORTF_DATA_BITS_R[0x06] = led;
+        if (!lock)
+        {
+            GPIO_PORTF_DATA_BITS_R[0x06] = led;
+        }
     }
 }
+
