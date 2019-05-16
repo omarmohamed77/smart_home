4509387bbfb620476a947ace9105dbf1bdbb4a9f
omarmagdy217 <36456356+omarmagdy217@users.noreply.github.com>
2019-05-16 22:11:07 +0200

    Indentation Organization

--- testFiles/Tiva_Home_Main/main.c
+++ testFiles/Tiva_Home_Main/main.c
@@ -2,14 +2,14 @@
 #include "Tiva_Home.h"
 
 #define Stepper_Read		0xC0
-#define PWM_Data			0x3F
+#define PWM_Data		0x3F
 
 void SystemInit(void){
 	UART7_init();
-    ADC0_Init();
-    Systick_init();
-    stepper_init();
-    PWM_Init();
+	ADC0_Init();
+	Systick_init();
+	stepper_init();
+	PWM_Init();
 }
 
 int main(void)
@@ -23,25 +23,24 @@ int main(void)
         avg = (Out_Data + Prev_temp) / 2;
         if (avg != preavg)
         {
-			//send
-            if((UART7_FR_R & 0x20)==0){
-	            UART7_DR_R = Out_Data;
-				Prev_temp = Out_Data;
-				preavg = avg;
-			}
+		//send
+		if((UART7_FR_R & 0x20)==0){
+			UART7_DR_R = Out_Data;
+			Prev_temp = Out_Data;
+			preavg = avg;
+		}
         }
         //receive
-		if((UART7_FR_R & 0x10)==0)
-		{
-			In_Data  = (UART7_DR_R & 0xFF);
-			Potentiometer_Reading = In_Data & PWM_Data;
-			PWM_Modulation(Potentiometer_Reading);
-            Stepper_Data = In_Data & Stepper_Read;
-            Stepper_Data >>= 6;
-            if (Stepper_Data == 0x01)
-                turn30_CC();
-            else if (Stepper_Data == 0x02)
-                turn30_CCW();
+	if((UART7_FR_R & 0x10)==0){
+		In_Data  = (UART7_DR_R & 0xFF);
+		Potentiometer_Reading = In_Data & PWM_Data;
+		PWM_Modulation(Potentiometer_Reading);
+		Stepper_Data = In_Data & Stepper_Read;
+		Stepper_Data >>= 6;
+		if (Stepper_Data == 0x01)
+			turn30_CC();
+		else if (Stepper_Data == 0x02)
+			turn30_CCW();
         }
     }
 }
